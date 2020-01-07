5979aedae33826dba4cacaac69c6fba1ff50ef42
Szalay <UK31680@itelligence.org>
2020-01-07 14:05:15 +0000

    fixed graphql issues

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,5 +1,12 @@
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
-const resolvers = {};
+const resolvers = {
+    Query:{
+        test: () => ["test1", "test2", "test3"]
+    },
+    Mutation: {
+        testm: () => ["test1", "test2", "test3"]
+    }
+};
 
 export default resolvers;
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,12 @@
 import {gql} from "apollo-server-express";
 
 const typeDefs = gql`
+    type Query {
+        test: [String]
+    }
+    type Mutation {
+        testm: [String]
+    }
 `;
 
 export default typeDefs;
