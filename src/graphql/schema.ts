3f38e4237022f5e604ced40ccaee2c0ff2f58def
Szalay <UK31680@itelligence.org>
2020-01-08 17:54:36 +0000

    started schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,50 @@
 import {gql} from "apollo-server-express";
 
 const typeDefs = gql`
+
+    type Language {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+    }
+
+    type ObjectDescription {
+        id: ID!
+        objectId: ID!
+        language: Language!
+        shortText: String!
+        longText: String
+    }
+
+    type Dice {
+        id: ID!
+        label: String!
+        minValue: Integer!
+        maxValue: Integer!
+    }
+
+    type Book{
+        id
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+    }
+
+    type Personality {
+        id
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+    }
+
+    type Race {
+        id
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String        
+    }
+
     type Query {
         test: [String]
     }
