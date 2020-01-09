5ea6914152d2175e2ac8c8c72a2caf498f44fbc6
Szalay <UK31680@itelligence.org>
2020-01-09 16:40:55 +0000

    schema changes

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,14 @@
-import {gql} from "apollo-server-express";
+import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
 
-    type Language {
+    interface DescribableObject {
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String        
+    }
+
+    type Language implements DescribableObject {
         id: ID!
         description: ObjectDescription!
         descriptions: [ObjectDescription]
@@ -17,6 +23,13 @@ const typeDefs = gql`
         longText: String
     }
 
+    type GlossaryItem implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String       
+    }
+
     type Dice {
         id: ID!
         label: String!
@@ -24,25 +37,73 @@ const typeDefs = gql`
         maxValue: Integer!
     }
 
-    type Book{
-        id
+    type Book implements DescribableObject {
+        id: ID!
         description: ObjectDescription!
         descriptions: [ObjectDescription]
         label: String
     }
 
-    type Personality {
-        id
+    type Personality implements DescribableObject {
+        id: ID!
         description: ObjectDescription!
         descriptions: [ObjectDescription]
         label: String
     }
 
-    type Race {
-        id
+    type Ability implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+    }
+
+    type Race implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+        classes: [Class]!        
+    }
+
+    type AgeGroup  implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String           
+    }
+
+    type MainClass implements DescribableObject {
+        id: ID!
         description: ObjectDescription!
         descriptions: [ObjectDescription]
         label: String        
+    }
+
+    type Class implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+        mainClass: MainClass!
+        subClasses: [SubClass]        
+    }
+
+    type SubClass implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+        class: Class!
+        specialisations: [SubClassSpecialisation]        
+    }
+
+    type SubClassSpecialisation implements DescribableObject {
+        id: ID!
+        description: ObjectDescription!
+        descriptions: [ObjectDescription]
+        label: String
+        subClass: SubClass!              
     }
 
     type Query {
